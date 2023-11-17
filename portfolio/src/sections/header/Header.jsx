d28e27c6e8379cb3bccb925fc3a944a1b2fd6f82
odfrank <oyedeledamilaref@yahoo.com>
2023-11-17 01:20:00 -0700

    Adds content and styles to the Header section with media queries

--- portfolio/src/sections/header/Header.jsx
+++ portfolio/src/sections/header/Header.jsx
@@ -1,8 +1,27 @@
+import HeaderImage from '../../assets/header.jpg'
+import HeaderData from './data'
 import './header.css'
 
 function Header() {
   return (
-    <header id="header"><h2>Header</h2></header>
+    <header id="header"> 
+      <div className="container header__container">
+        <div className="header__profile">
+          <img src={HeaderImage} alt="Header Portrait" />
+        </div>
+        <h3>DamiDev Expert</h3>
+        <p>You are highly welcome to my portfolio. I am a senior full-stack developer with expertise in C#/.Net and MERN platforms. Kindly reach out to me should you need any modern software solutions, and I will be very glad to help.</p>
+        <div className="header__cta">
+          <a href="#contact" className='btn primary'>Reach Out</a>
+          <a href="#portfolio" className='btn light'>My Projects</a>
+        </div>
+        <div className="header__socials">
+          {
+            HeaderData.map(item => <a key={item.id} href={item.link} target='_blank' rel='noopener noreferrer'>{item.icon}</a>)
+          }
+        </div>
+      </div>
+    </header>
   )
 }
 
