3bfc3f631f17c091c888b095a9aef4a994892678
odfrank <oyedeledamilaref@yahoo.com>
2023-11-16 18:31:36 -0700

    Adds all components to the main App.jsx file

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import Navbar from './sections/navbar/Navbar';
+import Header from './sections/header/Header';
+import About from './sections/about/About';
+import Services from './sections/services/Services';
+import Portfolio from './sections/portfolio/Portfolio';
+import Testimonials from './sections/testimonials/Testimonials';
+import Faqs from './sections/faqs/Faqs';
+import Contact from './sections/contact/Contact';
+import Footer from './sections/footer/Footer';
+import Floatingnav from './sections/floating-nav/Floatingnav';
 
 const App = () => {
   return (
-    <div>Hello There</div>
+    <main>
+      <Navbar />
+      <Header />
+      <About />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Faqs />
+      <Contact />
+      <Footer />
+      <Floatingnav />
+    </main>
   )
 }
 
