687adbb6233ee2164cb6cd459cbbd55670b14219
odfrank <oyedeledamilaref@yahoo.com>
2023-11-17 00:15:31 -0700

    Adds content and styles to the Nav Bar with media query for small screens

--- portfolio/src/sections/navbar/Navbar.jsx
+++ portfolio/src/sections/navbar/Navbar.jsx
@@ -1,8 +1,23 @@
+import Logo from '../../assets/logo.jpg'
+import MenuData from './data'
+import { HiColorSwatch } from "react-icons/hi";
 import './navbar.css';
 
 function Navbar() {
   return (
-    <nav><h2>Navbar</h2></nav>
+    <nav>
+      <div className="container nav__container">
+          <a href="index.html" className='nav__logo'>
+            <img src= {Logo} alt="Logo" />
+          </a>
+          <ul className='nav__menu'>
+            {
+              MenuData.map(item => <li key={item.id}><a href={item.link}>{item.title}</a></li>)
+            }
+          </ul>
+          <button id='theme__icon'><HiColorSwatch/></button>
+      </div>  
+    </nav>
   )
 }
 
